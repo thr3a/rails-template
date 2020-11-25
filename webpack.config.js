3762f187661dd150732b74c46d4cd96287c60bd0
thr3a <a@example.com>
2020-11-26 00:44:19 +0900

    add: webpack-dev-serverの追加

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,20 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "public/packs"),
-    publicPath: "/packs/",
+    publicPath: isProd ? "/packs/" : "//localhost:8081/packs/",
     filename: isProd ? "[name]-[hash].js" : "[name].js"
   },
   resolve: {
     extensions: [".js"]
   },
-  plugins: [new WebpackAssetsManifest({ publicPath: true })]
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/packs/",
+    host: "localhost",
+    port: 8081,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  plugins: [new WebpackAssetsManifest({ publicPath: true, writeToDisk: true })]
 };
